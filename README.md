1cafbf5962c0a49d8a6c6c39c43ef495dfc1f3a5
acubick <acubick@gmail.com>
2019-08-28 10:20:47 +0300

    start project

---
+++ README.md
@@ -0,0 +1,8 @@
+######  Luxury Travel
+
+`start project:`
+
+    "dev": "webpack --mode development",
+    "dev:watch": "webpack --mode development --watch",
+    "start": "webpack-dev-server --mode development",
+    "build": "webpack --mode production"
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'jquery'
+//import './css/index.css'
+import './scss/index.scss'
+import AppService from './modules/app.service'
+import { config } from './modules/config'
+
+
+//console.log('Config key:', config.key)
+//
+//const service = new AppService('Hello worggggSS!')
+//service.log()
+//
+//const $head = document.getElementsByTagName('header')
+//console.log('head jquery work',  $head )
+console.log( 'I am working' )
