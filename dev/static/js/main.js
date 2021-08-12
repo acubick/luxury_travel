0fef855d2a9204a47be21eaee6f3c154a36004d3
acubick <acubick@gmail.com>
2021-08-13 02:08:58 +0300

    Template ready

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,5 +1,101 @@
-
 // document.body.style.background = 'blue';
-$($ => {
-  console.log($, 'Hello')
+$(document).ready(function () {
+
+
+	let headerH = $('.header__menu').height(),
+			scrollPos = $(window).scrollTop(),
+		navH = $('.header').innerHeight();
+	checkScroll(scrollPos, headerH);
+	$(document).on('scroll', onScroll);
+
+	/* Fixed header when scroll
+	====================================*/
+
+
+	$(window).on('scroll resize', function () {
+
+		// headerH   = $('.header__menu').height();
+		scrollPos = $(this).scrollTop();
+		checkScroll(scrollPos, headerH);
+
+	});
+
+	function checkScroll(scrollPos, headerH) {
+		if (scrollPos > headerH) {
+			$('.navigation').addClass('fixed');
+			$('.intro').css({
+				'paddingTop': navH
+			});
+		} else {
+			$('.navigation').removeClass('fixed');
+			$('.header').removeAttr('style');
+		}
+	}
+
+
+
+//	подсветка пункта меню, в зависимости от текущего местоположения на странице
+	$(document).on('scroll', onScroll);
+
+	function onScroll(event) {
+		let scrollPos = $(document).scrollTop();
+		$('#nav a').each(function () {
+			let currLink = $(this),
+				refElement = $(currLink.attr('href'));
+			if (refElement.position().top <= (
+					scrollPos + navH
+				) && refElement.position().top + refElement.height() > (
+					scrollPos + navH
+				)) {
+				$('#nav  a').removeClass('active');
+				currLink.addClass('active');
+			} else {
+				currLink.removeClass('active');
+			}
+		});
+	}
+
+
+	// анимация перехода по id
+	$('#nav a').on('click', function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+		//забираем идентификатор блока с атрибута href
+		let id = $(this).attr('href'),
+			scrollPos = $(document).scrollTop(),
+			lenth = $(id).offset().top - (navH - 55),
+			distance = lenth - scrollPos,
+			time = 300,
+			count = Math.abs(distance / 1000);
+		// console.log('id  :>> ', id);
+		//  console.log('lenth :', lenth );
+		// console.log('scrollPos:', scrollPos);
+		// console.log( 'distance / 1000: ', distance / 1000 );
+
+		time *= parseInt(count);
+		if (time < 500) {
+			time += 700;
+		}
+
+		// console.log( 'count: ', parseInt(count) );
+		// console.log( 'time: ', time );
+		//анимируем переход на расстояние - top за 1000 мс
+		$('body,html').animate({
+			scrollTop: lenth
+		}, time);
+		$('.menu-burger__header, .header__menu').removeClass('open-menu');
+		$('body').removeClass('fixed-page');
+	});
+
+
+
+	$('.header__search').on('click', (e) => {
+		$('.find-tours').toggleClass('active')
+	})
+
+	$('.menu-burger__header').click(function () {
+		$('.menu-burger__header, .header__menu').toggleClass('open-menu');
+		// $('.header__menu').toggleClass('open-menu');
+		$('body').toggleClass('fixed-page');
+	});
 })
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const {
-    src,
-    dest,
-    watch,
-    parallel,
-    series
+	src,
+	dest,
+	watch,
+	parallel,
+	series
 } = require('gulp')
 const del = require('del')
 const gulpPug = require('gulp-pug')
@@ -23,6 +23,7 @@ const cheerio = require('gulp-cheerio')
 const replace = require('gulp-replace')
 const concat = require('gulp-concat')
 const gulpif = require('gulp-if')
+const gulpnotify = require('gulp-notify')
 
 let isBuildFlag = false
 
@@ -38,161 +39,175 @@ let isBuildFlag = false
 // }
 
 function cleanFolder() {
-    return del('dist')
+	return del('dist')
 }
 
 function fonts() {
-    return src(['dev/static/fonts/**/*.*'])
-        .pipe(dest('dist/static/fonts/'))
+	return src(['dev/static/fonts/**/*.*'])
+		.pipe(dest('dist/static/fonts/'))
 }
 
 
 function htmlInclude() {
-    return src(['dev/index.html'])
-        .pipe(gulpPlumber())
-        .pipe(fileInclude({
-            prefix: '@@',
-            basepath: '@file'
-        }))
-        .pipe(gulpRename({
-            basename: 'index',
-            extname: '.html'
-        }))
-        .pipe(gulpPlumber.stop())
-        .pipe(browserSync.stream())
-        .pipe(dest('dist/'))
-        // .pipe(browserSync.stream())
+	return src(['dev/index.html'])
+		.pipe(gulpPlumber())
+		.pipe(fileInclude({
+			prefix: '@@',
+			basepath: '@file'
+		}))
+		.pipe(gulpRename({
+			basename: 'index',
+			extname: '.html'
+		}))
+		.pipe(gulpPlumber.stop())
+		.pipe(browserSync.stream())
+		.pipe(dest('dist/'))
+	// .pipe(browserSync.stream())
 }
 
 
 function scss2css() {
-    return src('dev/static/styles/styles.scss')
-        .pipe(gulpPlumber())
-        .pipe(gulpSass())
-        .pipe(cleanCSS({
-            options: {
-                level: 2
-            }
-        }))
-        .pipe(autoprefixer())
-        .pipe(gulpPlumber.stop())
-        .pipe(browserSync.stream())
-        .pipe(dest('dist/static/css/'))
+	return src('dev/static/styles/styles.scss')
+		.pipe(gulpPlumber())
+		.pipe(gulpSass({
+			outputStyle: 'compressed'
+	}).on('error', gulpnotify.onError()))
+		.pipe(cleanCSS({
+			options: {
+				level: 2
+			}
+		}))
+		.pipe(autoprefixer())
+		.pipe(gulpPlumber.stop())
+		.pipe(browserSync.stream())
+		.pipe(dest('dist/static/css/'))
 
 }
 
 function script() {
-    return src('dev/static/js/main.js')
-        .pipe(gulpPlumber())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(gulpif(isBuildFlag, uglify()))
-        .pipe(gulpPlumber.stop())
-        .pipe(browserSync.stream())
-        .pipe(dest('dist/static/js/'))
+	return src('dev/static/js/main.js')
+		.pipe(gulpPlumber())
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(gulpif(isBuildFlag, uglify()))
+		.pipe(gulpPlumber.stop())
+		.pipe(browserSync.stream())
+		.pipe(dest('dist/static/js/'))
 
 }
 
 function vendors() {
-    return src([
-            './node_modules/svg4everybody/dist/svg4everybody.min.js',
-            './node_modules/slick-carousel/slick/slick.min.js'
-        ])
-        .pipe(gulpPlumber())
-        .pipe(concat('libs.js'))
-        .pipe(gulpPlumber.stop())
-        .pipe(dest('dist/static/js/vendors/'))
+	return src([
+			'./node_modules/svg4everybody/dist/svg4everybody.min.js',
+			'./node_modules/slick-carousel/slick/slick.min.js'
+		])
+		.pipe(gulpPlumber())
+		.pipe(concat('libs.js'))
+		.pipe(gulpPlumber.stop())
+		.pipe(dest('dist/static/js/vendors/'))
 
 }
 
 function copyLibs() {
-    return src(['./node_modules/jquery/dist/jquery.min.js'])
-        .pipe(dest('dist/static/js/vendors/'))
+	return src(['./node_modules/jquery/dist/jquery.min.js'])
+		.pipe(dest('dist/static/js/vendors/'))
 }
 
 
 function copyFavicons() {
-    return src(['./dev/static/icons/*.*'])
-        .pipe(dest('dist/static/icons/'))
+	return src(['./dev/static/icons/*.*'])
+		.pipe(dest('dist/static/icons/'))
 }
 
 
 function imageMin() {
-    return src(
-            'dev/static/images/**/*.{jpg,gif,png,svg}',
-            '!dev/static/images/sprite/*'
-        )
-        .pipe(imagemin([
-            imagemin.gifsicle({ interlaced: true }),
-            imagemin.mozjpeg({ quality: 75, progressive: true }),
-            imagemin.optipng({ optimizationLevel: 5 }),
-            imagemin.svgo({
-                plugins: [
-                    { removeViewBox: true },
-                    { cleanupIDs: false }
-                ]
-            })
-        ]))
-        .pipe(dest('dist/static/images/'))
+	return src(
+			'dev/static/images/**/*.{jpg,gif,png,svg}',
+			'!dev/static/images/sprite/*'
+		)
+		.pipe(imagemin([
+			imagemin.gifsicle({
+				interlaced: true
+			}),
+			imagemin.mozjpeg({
+				quality: 75,
+				progressive: true
+			}),
+			imagemin.optipng({
+				optimizationLevel: 5
+			}),
+			imagemin.svgo({
+				plugins: [{
+						removeViewBox: true
+					},
+					{
+						cleanupIDs: false
+					}
+				]
+			})
+		]))
+		.pipe(dest('dist/static/images/'))
 
 }
 
 
 function svgSpriteBuild() {
-    return src('dev/static/images/sprite/*.svg')
-        // minify svg
-        .pipe(svgmin({
-            js2svg: {
-                pretty: true
-            }
-        }))
-        // remove all fill, style and stroke declarations in out shapes
-        .pipe(cheerio({
-            run: function($) {
-                $('[fill]').removeAttr('fill')
-                $('[stroke]').removeAttr('stroke')
-                $('[style]').removeAttr('style')
-            },
-            parserOptions: { xmlMode: true }
-        }))
-        // cheerio plugin create unnecessary string '&gt;', so replace it.
-        .pipe(replace('&gt;', '>'))
-        // build svg sprite
-        .pipe(svgSprite({
-            mode: {
-                symbol: {
-                    sprite: 'sprite.svg'
-                }
-            }
-        }))
-        .pipe(dest('dist/static/images/sprite/'))
+	return src('dev/static/images/sprite/*.svg')
+		// minify svg
+		.pipe(svgmin({
+			js2svg: {
+				pretty: true
+			}
+		}))
+		// remove all fill, style and stroke declarations in out shapes
+		.pipe(cheerio({
+			run: function ($) {
+				$('[fill]').removeAttr('fill')
+				$('[stroke]').removeAttr('stroke')
+				$('[style]').removeAttr('style')
+			},
+			parserOptions: {
+				xmlMode: true
+			}
+		}))
+		// cheerio plugin create unnecessary string '&gt;', so replace it.
+		.pipe(replace('&gt;', '>'))
+		// build svg sprite
+		.pipe(svgSprite({
+			mode: {
+				symbol: {
+					sprite: 'sprite.svg'
+				}
+			}
+		}))
+		.pipe(dest('dist/static/images/sprite/'))
 }
 
 function setMode(isBuild) {
-    return cb => {
-        isBuildFlag = isBuild
-        cb()
-    }
+	return cb => {
+		isBuildFlag = isBuild
+		cb()
+	}
 }
 
 function watchingFiles() {
-    browserSync.init({
-        server: {
-            baseDir: 'dist'
-        },
-        notify: false,
-        open: false
-    })
-    watch('dev/*.html', htmlInclude)
-    watch('dev/static/styles/**/*.scss', scss2css)
-    watch('[dev/static/images/**/*.{jpg,gif,png,svg}, !dev/static/images/sprite/*.svg]', imageMin)
-    watch('dev/static/images/sprite/*', svgSpriteBuild)
-    watch('dev/static/js/main.js', script)
-    watch('dist/*.html').on('change', browserSync.reload)
+	browserSync.init({
+		server: {
+			baseDir: 'dist'
+		},
+		notify: false,
+		open: false
+	})
+	watch('dev/*.html', htmlInclude)
+	watch('dev/static/styles/**/*.scss', scss2css)
+	watch('[dev/static/images/**/*.{jpg,gif,png,svg}, !dev/static/images/sprite/*.svg]', imageMin)
+	watch('dev/static/images/sprite/*', svgSpriteBuild)
+	watch('dev/static/js/main.js', script)
+	watch('dist/*.html').on('change', browserSync.reload)
 }
 
-const dev = parallel(fonts, htmlInclude, scss2css, imageMin, svgSpriteBuild, vendors, script, copyLibs)
+const dev = parallel(fonts, htmlInclude, scss2css, imageMin, svgSpriteBuild, vendors, script, copyLibs, copyFavicons)
 
 exports.default = series(cleanFolder, dev, watchingFiles)
-exports.build = series(cleanFolder, setMode(true), dev, copyFavicons)
+exports.build = series(cleanFolder, setMode(true), dev)
